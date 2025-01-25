243bada30a90f89e5bf0903f1fe8da46c1cb078c
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2025-01-25 10:28:56 -0500

    Processes each dataset in configuration for data cleansing.

--- aud_cleanse.py
+++ aud_cleanse.py
@@ -2,11 +2,35 @@ import pandas as pd
 import os
 import json
 
+# Loads configuration settings
 with open("config.json", "r") as file:
     config = json.load(file)
 
+# Gets folder paths from configuration (config.json)
 raw_data_folder = config["raw_data_folder"]
 processed_data_folder = config["processed_data_folder"]
 
+# Makes sure processed data folder exists
 if not os.path.exists(processed_data_folder):
     os.makedirs(processed_data_folder)
+
+# Processes each dataset for configuration
+for dataset in config["AUTDProjectFiles"]:
+    input_file = os.path.join(raw_data_folder, dataset["input_file"])
+    output_file = os.path.join(processed_data_folder, dataset["output_file"])
+
+    try:
+        print(f"Processing {input_file}...")
+        
+        # Handles duplicate records, missing values left as null for better accuracy in future analysis
+        df = pd.read_csv(input_file)
+        df = df.drop_duplicates()
+        df.to_csv(output_file, index=False)
+        print(f"Processed data saved to {output_file}\n")
+
+
+    # Handles errors and leaves message for reasoning behind error.
+    except FileNotFoundError:
+        print(f"Error: {input_file} not found. Please ensure this file exists in the raw data folder.\n")
+    except Exception as e:
+        print(f"An error occurred while processing {input_file}: {e}\n")
