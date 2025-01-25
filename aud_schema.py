c08526377d2b19f125d8a3855922be2036c5b487
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2025-01-25 15:25:27 -0500

    Created tables match_data and salary_data within the database and added required columns based on processed data.

--- aud_schema.py
+++ aud_schema.py
@@ -1,7 +1,41 @@
 import sqlite3
 
 connect = sqlite3.connect("aud_project_data.db")
+cursor = connect.cursor()
 
-connect.close()
+create_match_data_table = """
+CREATE TABLE IF NOT EXISTS match_data (
+    match_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_date TEXT,
+    game_time TEXT,
+    home_team TEXT,
+    h_goals INTEGER,
+    h_xg REAL,
+    hplayer_xg REAL,
+    away_team TEXT,
+    a_goals INTEGER,
+    a_xg REAL,
+    aplayer_xg REAL,
+    goal_diff INTEGER,
+    x_gd REAL,
+    player_xgd REAL,
+    final_diff INTEGER,
+    h_xpoints REAL,
+    a_xpoints REAL
+);
+"""
 
-print("Database created successfully.")
+create_salary_data_table = """
+CREATE TABLE IF NOT EXISTS salary_data (
+    player TEXT,
+    team TEXT,
+    season INTEGER,
+    position TEXT,
+    base_salary REAL,
+    guaranteed REAL,
+    date TEXT
+
+);
+"""
+
+
