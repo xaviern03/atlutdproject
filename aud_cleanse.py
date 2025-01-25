a86ce1d424cfacdb78eacf879025f569a7d6e208
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2025-01-25 10:11:53 -0500

    import required modules, Load config.json file for the file paths and configuration

--- aud_cleanse.py
+++ aud_cleanse.py
@@ -1,6 +1,12 @@
 import pandas as pd
-import glob
-import json
 import os
+import json
 
+with open("config.json", "r") as file:
+    config = json.load(file)
 
+raw_data_folder = config["raw_data_folder"]
+processed_data_folder = config["processed_data_folder"]
+
+if not os.path.exists(processed_data_folder):
+    os.makedirs(processed_data_folder)
